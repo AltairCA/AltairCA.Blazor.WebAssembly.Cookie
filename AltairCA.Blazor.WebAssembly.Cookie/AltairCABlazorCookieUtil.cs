6921be1225352db3838e380622fa0a91193dd929
AltairCA <alexvista1234@gmail.com>
2022-04-11 12:27:11 +0530

    mid

--- AltairCA.Blazor.WebAssembly.Cookie/AltairCABlazorCookieUtil.cs
+++ AltairCA.Blazor.WebAssembly.Cookie/AltairCABlazorCookieUtil.cs
@@ -8,8 +8,28 @@ namespace AltairCA.Blazor.WebAssembly.Cookie
 {
     public interface IAltairCABlazorCookieUtil
     {
+        /// <summary>
+        /// Set a object in the cookie
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <param name="span"></param>
+        /// <param name="path"></param>
+        /// <param name="domain"></param>
+        /// <param name="secure"></param>
+        /// <returns></returns>
         Task SetValueAsync(string key, object value, TimeSpan? span = null, string path = null,
             string domain = null, bool? secure = null);
+        /// <summary>
+        /// Set a string in the cookie
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <param name="span"></param>
+        /// <param name="path"></param>
+        /// <param name="domain"></param>
+        /// <param name="secure"></param>
+        /// <returns></returns>
         Task SetValueAsync(string key, string value, TimeSpan? span = null,string path=null,string domain=null,bool? secure = null);
         Task<string> GetValueAsync(string key);
         Task<T> GetValueAsync<T>(string key) where T : class;
